9447a694adfd52e705b23e1509d7f332581e0b0f
kekland <kk.erzhan@gmail.com>
2026-04-18 11:38:19 +0500

    fix: linker issues (c++ name mangling)

--- src/croppy.c
+++ src/croppy.c
@@ -2,12 +2,12 @@
 #include "croppy.h"
 #include "fit_polygon_in_quad.h"
 
-FFI_PLUGIN_EXPORT Aabb2 fit_polygon_in_quad(double *points, int length)
+FFI struct Aabb2 fit_polygon_in_quad(double *points, int length)
 {
 	return fit_polygon_in_quad_impl(points, length);
 }
 
-FFI_PLUGIN_EXPORT Aabb2 fit_polygon_in_quad_on_resize(double *points,
+FFI struct Aabb2 fit_polygon_in_quad_on_resize(double *points,
 																											int length,
 																											double aspectRatio,
 																											bool isTopLeftStatic,
--- src/croppy.h
+++ src/croppy.h
@@ -1,12 +1,14 @@
 #ifndef CROPPY_H
 #define CROPPY_H
 
+// Common includes
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
-#if _WIN32
+// Windows-specific includes
+#if defined(_WIN32)
 #define NOMINMAX
 #include <windows.h>
 #else
@@ -14,11 +16,20 @@
 #include <unistd.h>
 #endif
 
-#if _WIN32
-#define FFI_PLUGIN_EXPORT __declspec(dllexport)
+// Export attributes
+#if defined(_WIN32)
+#define EXPORT __declspec(dllexport)
 #else
-#define FFI_PLUGIN_EXPORT
+#define EXPORT __attribute__((__visibility__("default"))) __attribute__((__used__))
 #endif
+
+#ifdef __cplusplus
+  #define EXTERNC extern "C"
+#else
+  #define EXTERNC
+#endif
+
+#define FFI EXTERNC EXPORT
 
 struct Vector2
 {
@@ -36,9 +47,9 @@ struct Aabb2
 
 typedef struct Aabb2 Aabb2;
 
-FFI_PLUGIN_EXPORT Aabb2 fit_polygon_in_quad(double *points, int length);
+FFI struct Aabb2 fit_polygon_in_quad(double *points, int length);
 
-FFI_PLUGIN_EXPORT Aabb2 fit_polygon_in_quad_on_resize(double *points,
+FFI struct Aabb2 fit_polygon_in_quad_on_resize(double *points,
                                                       int length,
                                                       double aspectRatio,
                                                       bool isTopLeftStatic,
